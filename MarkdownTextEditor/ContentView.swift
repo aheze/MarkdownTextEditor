0cc21809fca695f45c44366a1879dd096c8c460e
Zheng <aheze@getfind.app>
2021-06-09 12:02:42 -0700

    Initial Commit

---
+++ MarkdownTextEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarkdownTextEditor
+//
+//  Created by Zheng on 6/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkdownTextEditor/MarkdownTextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownTextEditorApp.swift
+//  MarkdownTextEditor
+//
+//  Created by Zheng on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownTextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
