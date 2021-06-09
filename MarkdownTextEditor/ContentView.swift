13ba6133d09900948c681e9b9775ec2b96b95774
Zheng <aheze@getfind.app>
2021-06-09 12:20:13 -0700

    Base

--- MarkdownTextEditor/ContentView.swift
+++ MarkdownTextEditor/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var markdownText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 0) {
+            
+            Text("Markdown")
+            TextEditor(text: $markdownText)
+                .frame(height: 400)
+                .border(Color.blue)
+            
+            Text("Output")
+            Text(getAttributedString(markdown: markdownText))
+                .frame(maxWidth: .infinity, minHeight: 100, alignment: .topLeading)
+                .border(Color.blue)
+            
+            Spacer()
+        }
+        .font(.title2)
+        .padding()
+    }
+    
+    func getAttributedString(markdown: String) -> AttributedString {
+        do {
+            let attributedString = try AttributedString(markdown: markdown)
+            return attributedString
+        } catch {
+            print("Couldn't parse: \(error)")
+        }
+        return AttributedString("Error parsing markdown")
     }
 }
 
